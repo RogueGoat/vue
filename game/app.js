a188ef364078171ae6a7f0c6668d303c8bcb6baa
Sam Nelson <sam@brownboots.com>
2018-08-15 15:17:59 -0500

    heal and special attack with reformatted code

--- game/app.js
+++ game/app.js
@@ -12,35 +12,55 @@ new Vue({
       this.monsterHealth = 100;
     },
     attack: function() {
-      var max = 10;
-      var min = 3;
-      var damage = Math.max(Math.floor(Math.random() * max) + 1, min);
-      this.monsterHealth -= damage;
-
-      if (this.monsterHealth <= 0) {
-        alert('you won!');
-        this.gameIsRunning = false;
+      this.monsterHealth -= this.calculateDamage(3, 10);
+      if (this.checkWin()) {
         return;
       }
-
-      max = 12;
-      min = 5;
-      var damage = Math.max(Math.floor(Math.random() * max) + 1, min);
-      this.playerHealth -= damage;
-
-      if (this.playerHealth <= 0) {
-        alert('you lose!');
-        this.gameIsRunning = false;
-      }
+      this.monsterAttacks();
     },
     specialAttack: function() {
-
+      this.monsterHealth -= this.calculateDamage(10, 20);
+      if (this.checkWin()) {
+        return;
+      }
+      this.monsterAttacks();
     },
     heal: function() {
+      if (this.playerHealth <= 90) {
+        this.playerHealth += 10;
+      } else {
+        this.playerHealth = 100;
+      }
 
+      this.monsterAttacks();
     },
     giveUp: function() {
 
+    },
+    monsterAttacks: function(){
+      this.playerHealth -= this.calculateDamage(5, 12);
+      this.checkWin();
+    },
+    calculateDamage: function(min, max) {
+      return Math.max(Math.floor(Math.random() * max) + 1, min);
+    },
+    checkWin: function() {
+      if (this.monsterHealth <= 0) {
+        if (confirm('You won! New game?')) {
+          this.startGame();
+        } else {
+          this.gameIsRunning = false;
+        }
+        return true;
+      } else if (this.playerHealth <= 0) {
+        if (confirm('You lost! New game?')) {
+          this.startGame();
+        } else {
+          this.gameIsRunning = false;
+        }
+        return true;
+      }
+      return false;
     }
   }
 });
