8c32adc4e48eb5d2c35d92036e85e85cf39d4908
Sam Nelson <sam@brownboots.com>
2018-08-16 14:37:32 -0500

    how vue works behind the scenes

--- instance/app2.js
+++ instance/app2.js
@@ -2,5 +2,34 @@ new Vue({
   el: '#app',
   data: {
     title: 'The VueJS Instance'
+  },
+  beforeCreate: function() {
+    console.log('beforeCreate()');
+  },
+  created: function() {
+    console.log('created()');
+  },
+  beforeMount: function() {
+    console.log('beforeMount()');
+  },
+  mounted: function() {
+    console.log('mounted()');
+  },
+  beforeUpdate: function() {
+    console.log('beforeUpdate()');
+  },
+  updated: function() {
+    console.log('updated()');
+  },
+  beforeDestroy: function() {
+    console.log('beforeDestroy()');
+  },
+  destroyed: function() {
+    console.log('destroyed()');
+  },
+  methods: {
+    destroyed: function() {
+      this.$destroy();
+    }
   }
 });
